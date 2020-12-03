9bde600c87a5ebb94bec35d42bf26d6749788bf8
neupane11 <sandip.neupane11@gmail.com>
2020-12-03 01:37:47 -0700

    changes

--- front_end/src/components/Eachtransaction.js
+++ front_end/src/components/Eachtransaction.js
@@ -6,7 +6,7 @@ export const Eachtransaction=({transaction})=>{
     const {deleteTransaction} = useContext(GlobalContext)
     const sign= transaction.amount<0?'-':'+'
     return(
-    <li className={transaction.amount<0?"list-group-item list-group-item-minus":"list-group-item list-group-item-plus"}>{transaction.text} <span style={{float:"right"}}>{sign}${Math.abs(transaction.amount)}</span >
+    <li className={transaction.amount<0?"list-group-item list-group-item-minus":"list-group-item list-group-item-plus"}>{transaction.text} <span style={{float:"right",color:"red"}}>{Math.abs(transaction.amount)}</span >
                 <button onClick={()=>deleteTransaction(transaction._id)} className="deletebtn" >x</button></li>
     )
 }
